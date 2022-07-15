a00df1acdbff41db452a67be493babe71d400028
youssofOkiel <y.okiel@bimsaudi.com>
2022-07-15 23:12:17 +0200

    tweets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainComponent } from './components/main/main.component';
 import {NgCircleProgressModule} from 'ng-circle-progress'
 import { HttpClientModule } from '@angular/common/http';
+import { TweetsComponent } from './components/tweets/tweets.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MainComponent
+    MainComponent,
+    TweetsComponent
   ],
   imports: [
   BrowserModule,

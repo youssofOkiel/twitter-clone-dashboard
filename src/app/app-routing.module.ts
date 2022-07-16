51b110cb4b07430c5bb93776980eb787bae0d0e4
MN2255 <105072409+MN2255@users.noreply.github.com>
2022-07-16 20:27:08 +0200

    notfound component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
-  {path:"main" , component:MainComponent},
-  {path:"" , component:MainComponent},
+  {path:"" , component:MainComponent,children:[
+    {path:"main" , component:MainComponent},
+    
+  ]},
+  
+  {path:"**" , component:NotfoundComponent},
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
